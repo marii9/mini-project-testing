14bbcb8ae0583fc1450368e93ac6d5d5590eaa1d
marii9 <116024194+marii9@users.noreply.github.com>
2023-03-04 23:25:03 -0500

    Update homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,7 +14,16 @@ router.get('/', async (req, res) => {
       ],
     });
 
-
+    router.get('/signup', (req, res) => {
+      // If the user is already logged in, redirect the request to another route
+      if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+      }
+    
+      res.render('signup');
+    });
+    
     
 
     // Serialize data so the template can read it
